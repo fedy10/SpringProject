c6733903b7351c1223a4edf63824c9053a5d5142
fedy10 <94867076+fedy10@users.noreply.github.com>
2023-12-10 00:07:01 +0100

    mais les relations ( many to one , one to many , heritage)

--- src/main/java/com/example/springproject/entities/AccountOperation.java
+++ src/main/java/com/example/springproject/entities/AccountOperation.java
@@ -1,16 +1,20 @@
 package com.example.springproject.entities;
 
 import com.example.springproject.enums.OperationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+@Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 public class AccountOperation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  long id;
     private Date operationDate;
     private double amount;
     private OperationType type;
+    @ManyToOne
     private BankAccount bankAccount;
 }
