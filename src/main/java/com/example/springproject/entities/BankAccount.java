a1e9f94ec374987e0dcabd2fc620d56e3c00ef1c
fedy10 <94867076+fedy10@users.noreply.github.com>
2023-12-10 00:40:55 +0100

    basculer de la methdoe de heritage single table vers table_per_class

--- src/main/java/com/example/springproject/entities/BankAccount.java
+++ src/main/java/com/example/springproject/entities/BankAccount.java
@@ -9,10 +9,11 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 import java.util.List;
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)//pour l'heritage
-@DiscriminatorColumn(name = "TYPE",length = 4,discriminatorType = DiscriminatorType.STRING)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)//pour l'heritage
+//@DiscriminatorColumn(name = "TYPE",length = 4,discriminatorType = DiscriminatorType.STRING)
 @Data @NoArgsConstructor @AllArgsConstructor
-public class BankAccount {
+public abstract class BankAccount {
+    //abstract pour ne pas creer un table bankAccount
     @Id
     private String id;
     private double balance;
